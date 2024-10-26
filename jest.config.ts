82ff814d1438968fea83730bdb914823771d3c77
Violet Kiseleva <eerfeer@ya.ru>
2024-10-26 19:29:56 +0300

    change ts-jest to @swc/jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,8 @@
 export default {
   coverageProvider: 'v8',
-  preset: 'ts-jest',
   testEnvironment: 'node',
   reporters: ['default', ['github-actions', { silent: false }], 'summary'],
-  transform: { '^.+\\.(ts|tsx)$': 'ts-jest' },
+  transform: { '^.+\\.(ts|tsx)$': '@swc/jest' },
   verbose: true,
   moduleNameMapper: {
     'src/(.*)$': '<rootDir>/src/$1'
